06c876b75df34b0336b82b37ad313d675e802deb
Lynn Cyrin <firemagelynn@gmail.com>
2016-10-12 10:12:34 -0700

    remove logging

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@ function autolink(match) {
   $(match).each(function(index, element) {
 
     element_id = match.replace('>', '').replace(/\s+/, '') + index
-    console.log(element_id)
     $(element).attr('id', element_id);
     $(element).prepend('<a class="autolink" href="#'+element_id+'"><i class="fa fa-link"></i></a>')
 
