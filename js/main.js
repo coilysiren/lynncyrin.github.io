dd7d252a3bf10710164ecb36cc51dc6f0eca4b73
Lynn Cyrin <firemagelynn@gmail.com>
2016-10-12 09:56:26 -0700

    autolink script

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
+function autolink(match) {
+  $(match).each(function(index, element) {
+
+    element_id = match.replace('>', '').replace(/\s+/, '') + index
+    console.log(element_id)
+    $(element).attr('id', element_id);
+    $(element).prepend('<a class="autolink" href="#'+element_id+'"><i class="fa fa-link"></i></a>')
+
+  });
+}
+
 $( document ).ready(function() {
-    console.log( "ready!" );
+  autolink('ol >')
 });
