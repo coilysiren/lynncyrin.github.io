c5264ddcbf55ee42dba79bd6f57338ccf498238e
Lynn Cyrin <firemagelynn@gmail.com>
2016-10-12 10:16:54 -0700

    resolve homepage inifite loop issue >_>

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 function autolink(match) {
   $(match).each(function(index, element) {
 
-    element_id = match.replace('>', '').replace(/\s+/, '') + index
+    element_id = (match+index).replace('>', '').replace(/\s+/, '').replace(/\s+/, '')
     $(element).attr('id', element_id);
     $(element).prepend('<a class="autolink" href="#'+element_id+'"><i class="fa fa-link"></i></a>')
 
@@ -9,6 +9,6 @@ function autolink(match) {
 }
 
 $( document ).ready(function() {
-  autolink('ol >')
+  autolink('article ol >')
   window.location.href = window.location.hash
 });
